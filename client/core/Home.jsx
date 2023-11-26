5dfc874c5951d1a7e997ce154e390463c4d976c6
Extraordinary Sheep <127700995+ExtraordinarySheep@users.noreply.github.com>
2023-11-26 16:40:44 -0500

    Add files via upload
    core files

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -28,36 +28,40 @@ import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
-import unicornbikeImg from './../assets/images/unicornbikeImg.jpg';
+import onlineshopping3 from './../assets/images/Picture1.jpg';
+
    
 
 const useStyles = makeStyles(theme => ({
   card: {
-    maxWidth: 600,
+    maxWidth: 800,
     margin: 'auto',
-    marginTop: theme.spacing(5),
+    marginTop: theme.spacing(3),
   },
   title: {
     padding: theme.spacing(3, 2.5, 2),
     color: theme.palette.openTitle,
   },
   media: {
-    minHeight: 400,
+    minHeight: 1066,
   },
+
+ 
 }));
 
 export default function Home(){ 
 const classes = useStyles()
 return (
 <Card className={classes.card}>
-   
-  <Typography variant="h6" className={classes.title}>Home Page</Typography>
+   <center>
+  <Typography variant="h3" className={classes.title}>Home Page</Typography>
+  <Typography variant="body2" component="p"> 
+Welcome to SmartWeb Application
+</Typography></center>
 <CardMedia className={classes.media}
-image={unicornbikeImg} title="Unicorn Bicycle"/>
+image={onlineshopping3} title="online shopping"/>
 <CardContent>
-<Typography variant="body2" component="p"> 
-Welcome to the MERN Skeleton home page.
-</Typography> 
+
 </CardContent>
 </Card> 
 )
@@ -65,6 +69,13 @@ Welcome to the MERN Skeleton home page.
 
 /*const MyComponent = () => {
   const classes = useStyles();
+
+
+  <Typography variant="body2" component="p"> 
+Welcome to SmartWeb Application.
+</Typography> 
+
+
 
   return (
     <Card className={classes.card}>
